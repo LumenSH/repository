f5d5ec6716638215da8a01c54716f5abf6ee3298
Shyim <shyim@posteo.de>
2017-11-12 18:03:34 +0000

    Added basic build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,16 @@
 #/bin/bash
 
+# Build packages
+echo "Building packages"
 
+for filename in ./packages/*; do
+	cd $filename
+	make
+	cd ../..
+done
+
+echo "Building repository"
+
+# Build repository
+cd repository
+bash .generate.sh
