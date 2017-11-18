bc48b47e20b8ff4fb58f3e71f4293942ad6d12ef
Soner Sayakci <s.sayakci@gmail.com>
2017-11-18 19:58:12 +0000

    Added i386

--- repository/.generate.sh
+++ repository/.generate.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+# amd64
 apt-ftparchive -c .dev_repo packages pool/stable/binary-amd64 > dists/stable/main/binary-amd64/Packages
 gzip -fk dists/stable/main/binary-amd64/Packages
 rm -rf dists/stable/main/binary-amd64/Release
 rm -rf dists/stable/main/binary-amd64/Release.gpg
 apt-ftparchive -c .dev_repo release dists/stable/main/binary-amd64 > dists/stable/main/binary-amd64/Release
+
+# i386
+apt-ftparchive -c .dev_repo packages pool/stable/binary-i386 > dists/stable/main/binary-i386/Packages
+gzip -fk dists/stable/main/binary-i386/Packages
+rm -rf dists/stable/main/binary-i386/Release
+rm -rf dists/stable/main/binary-i386/Release.gpg
+apt-ftparchive -c .dev_repo release dists/stable/main/binary-i386 > dists/stable/main/binary-i386/Release
+
 rm -rf dists/stable/Release
 rm -rf dists/stable/Release.gpg
 apt-ftparchive -c .dev_repo release dists/stable> dists/stable/Release
+
