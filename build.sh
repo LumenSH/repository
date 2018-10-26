01a4919f3a871c1eb85a402f9a49a327cb3ff438
Shyim <shyim@posteo.de>
2018-10-26 20:44:27 +0200

    Add pacman support

--- build.sh
+++ build.sh
@@ -1,36 +1,42 @@
-#/bin/bash
+#!/usr/bin/env bash
+
+CONTAINER_NAME="arch"
 
 case $1 in
-    all)
-	./build.sh cleanup
-	echo "Building all packages"
-        for dir in ./packages/*; do
-		echo "Building $dir"
-		./build.sh $dir
-        done
-        cd /repository
-        find ./packages/ -name "*.deb" -type f -a -not -path '*.gitkeep*' -exec mv {} /repository/repository/pool/stable/binary-amd64/ \;
-
-	./build.sh sign
-	./build.sh cleanup
-	;;
     sign)
-	find ./packages/ -name "*.deb" -type f -a -not -path '*.gitkeep*' -exec mv {} /repository/repository/pool/stable/binary-amd64/ \;
-	# remove already existing files
-	find ./repository/dists -type f -a -not -path '*.gitkeep*' -exec rm {} \;
+        find ./packages/ -name "*.deb" -type f -a -not -path '*.gitkeep*' -exec mv {} /repository/public/debian/pool/stable/binary-amd64/ \;
+        find ./public/debian/dists -type f -a -not -path '*.gitkeep*' -exec rm {} \;
 
-	echo "Building repository"
-	cd repository
-	./.generate.sh
+        echo "Building repository"
+        cd /repository/public/debian
+        ./.generate.sh
+	;;
+	pkgbuild)
+	    find ./packages/ -name "*.pkg.tar.xz" -type f -a -not -path '*.gitkeep*' -exec mv {} /repository/public/archlinux/ \;
+	    docker exec ${CONTAINER_NAME} "cd /arch/ && repo-add lumen.db.tar.gz /arch/*.pkg.tar.xz"
 	;;
     cleanup)
-	echo "Cleanup packages"
-	git checkout -- packages/
-	git clean -fd packages/
-	;;
+        echo "Cleanup packages"
+        git checkout -- packages/
+        git clean -fd packages/
+    ;;
     *)
-        cd $1
-	make
-        ;;
-esac
+        PACKAGE_NAME="$1"
 
+        if [ -d "./packages/debian/${PACKAGE_NAME}" ]; then
+            cd "./packages/debian/${PACKAGE_NAME}"
+            make TARGET="deb"
+        fi
+
+        if [ -d "./packages/archlinux/${PACKAGE_NAME}" ]; then
+            cd "./packages/archlinux/${PACKAGE_NAME}"
+            make TARGET="pacman"
+        fi
+
+        if [ -d "./packages/shared/${PACKAGE_NAME}" ]; then
+            cd "./packages/shared/${PACKAGE_NAME}"
+            make TARGET="deb"
+            make TARGET="pacman"
+        fi
+    ;;
+esac
